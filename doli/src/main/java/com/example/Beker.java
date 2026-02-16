d9ba0a50c9b127fff025b468ecc6dc4b62697d41
El Miri Martin <bsbebgcst@gmail.com>
2026-02-16 16:09:54 +0100

    órai feladat

---
+++ doli/src/main/java/com/example/Beker.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Beker {
+
+    public static void task01(){
+
+        Integer[] osszPontszam = new Integer[5];
+        int pont = -1;
+
+        try(Scanner sc = new Scanner(System.in)){
+            
+            for(int i = 1; i<=5; i++){
+
+                do{
+
+                    System.out.printf("%d. feladatban elért pontszám: ", i);
+                    String pontStr = sc.nextLine();
+
+                    try{pont = Integer.parseInt(pontStr);}
+                    catch(Exception e){
+
+                        System.err.println("Hibás adat! ez nem szám ");
+                    }
+
+                    if(pont < 0 || pont > 10){
+
+                        System.err.println("Hibás adat! nem 0 és 10 között van a szám ");
+                    }else{
+                        
+                        osszPontszam[i - 1] = pont;
+                    }
+                } while(pont < 0 || pont > 10);
+            }
+            int ossz = 0;
+            for(int szam : osszPontszam){
+
+                ossz += szam;
+            }
+            System.out.printf("Az elért összpontszám: %d pont \n", ossz); 
+
+        }
+    }
+}
+
---
+++ doli/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("doli cucc");
+
+        Beker.task01();
+    }
+}
